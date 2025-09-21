c8fc00ee1c0a3ae6bdc19720a4a09264b73fbcd6
nada29348-hash <nada29348@gmail.com>
2025-09-21 16:15:19 +0200

    change theme and create notes view and notes view body

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: NotesViewBody());
+  }
+}
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
