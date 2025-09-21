8c771463c9e04cb73fc5b9875833c54db5d4faa3
nada29348-hash <nada29348@gmail.com>
2025-09-21 16:36:30 +0200

    create custom app bar and custom search icon bar

---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app2/widgets/custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: const Column(children: [SizedBox(height: 60), CustomAppBar()]),
+    );
+  }
+}
